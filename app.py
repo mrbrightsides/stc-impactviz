9dc42e497f2667a91e5551d98edcd94776b8b20e
Akhmad Khudri <khudri@binadarma.ac.id>
2025-09-25 21:36:52 +0700

    Adjust bottom padding in embed_iframe call

--- app.py
+++ app.py
@@ -97,4 +97,4 @@ def embed_iframe(src, hide_top_px=100, hide_bottom_px=0, height=800):
 iframe_url = "https://ohara.ai/mini-apps/aa4ac443-679c-480c-90cd-62933ae44dc5"
 
 # Panggil fungsi
-embed_iframe(iframe_url, hide_top_px=110, hide_bottom_px = 20, height=800)
+embed_iframe(iframe_url, hide_top_px=110, hide_bottom_px = 80, height=800)
